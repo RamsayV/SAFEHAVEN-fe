9ee02514cccb1acf98bae61ae5e1c3d24c2135f6
Ramsay <ramsayveitch123@gmail.com>
2024-04-10 17:02:09 +0100

    changes for deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom"
-import { useAppContext } from "../contexts/AppContext"
-import SignOutButton from "./SignOutButton"
+
 
 const Header = () => {
-  const {isLoggedIn} = useAppContext()
   return (
     <div className='bg-red-400 py-6'>
         <div className='container mx-auto flex justify-between'>
             <span className='text-3xl text-white font-bold tracking-tight'>
               <Link to="/">SAFEHAVEN</Link>  
             </span>
-            {/* <span className='flex space-x-2'>
+            {/* <span c'>
               {isLoggedIn ? <>
               <Link to="/nothing" className="flex items-center text-red-400 px-3 font-bold bg-white hover:bg-gray-100">
                 Chats
--- src/pages/Toilets.tsx
+++ src/pages/Toilets.tsx
@@ -1,17 +1,16 @@
-import { useState } from "react";
 import ToiletApi from "../components/Toilets/ToiletApi";
 import Navbar from "../components/Navbar";
 
 
 
-interface Toilet {
-  name: string;
-  street: string;
-  city: string;
-  state: string;
-  ada: boolean;
-  unisex: boolean;
-}
+// interface Toilet {
+//   name: string;
+//   street: string;
+//   city: string;
+//   state: string;
+//   ada: boolean;
+//   unisex: boolean;
+// }
 
 const Toilets = () => {
   return (
